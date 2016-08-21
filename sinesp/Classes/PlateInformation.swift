0f8622149ece292b5f419666a56f280b72375c2d
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-08-20 23:19:08 -0300

    Add PlateInformation

---
+++ sinesp/Classes/PlateInformation.swift
@@ -0,0 +1,16 @@
+struct PlateInformation {
+    let returnCode: String
+    let returnMessage: String
+    let statusCode: String
+    let statusMessage: String
+    let chassis: String
+    let model: String
+    let brand: String
+    let color: String
+    let year: Int
+    let modelYear: Int
+    let plate: Plate
+    let date: NSDate
+    let city: String
+    let state: String
+}
