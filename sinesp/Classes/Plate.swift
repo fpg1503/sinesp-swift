e21e2283d4f84ea6946a980b37940e4ca1aa636d
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-08-21 16:03:52 -0300

    Add Plate validation

--- sinesp/Classes/Plate.swift
+++ sinesp/Classes/Plate.swift
@@ -1,7 +1,31 @@
+import Regex_swift
+
+private extension String {
+    var stringByRemovingWhitespaces: String {
+        let components = componentsSeparatedByCharactersInSet(.whitespaceCharacterSet())
+        return components.joinWithSeparator("")
+    }
+}
+
 public struct Plate {
     public init?(plate: String) {
-        //TODO: Validate plate
-        self.plate = plate
+        guard let regex = Regex(pattern: "^(\\w{3})-?(\\d{4})$") else { return nil }
+        let cleanPlate = plate.uppercaseString.stringByRemovingWhitespaces
+
+        guard let captureGroups = regex.matches(cleanPlate).first?.captureGroups
+            where captureGroups.count == 2 else { return nil }
+
+        let lettersString = captureGroups[0]
+        let numbersString = captureGroups[1]
+
+        letters = lettersString.characters.map { $0 as Character }
+        numbers = numbersString.characters.map { $0 as Character }.map { String($0) }.map{ Int($0) }.flatMap { $0 }
+
+        self.plate = lettersString + numbersString
     }
+
+    let letters: [Character]
+    let numbers: [Int]
+
     let plate: String
 }
