e0a1b71072248a096c66b9c309564e927a5ab643
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-08-21 13:52:01 -0300

    Add AEXML to Example

--- Example/Pods/Target Support Files/Pods-sinesp_Tests/Pods-sinesp_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-sinesp_Tests/Pods-sinesp_Tests-frameworks.sh
@@ -84,10 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/AEXML/AEXML.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Arcane/Arcane.framework"
   install_framework "$BUILT_PRODUCTS_DIR/sinesp/sinesp.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/AEXML/AEXML.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Arcane/Arcane.framework"
   install_framework "$BUILT_PRODUCTS_DIR/sinesp/sinesp.framework"
 fi
