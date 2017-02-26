0ffc5575b1c38ffd80b40bfdf7b13ab8af86e033
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2017-02-26 11:11:05 -0300

    Use expectations so test actually runs

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -5,9 +5,12 @@ import sinesp
 class Tests: XCTestCase {
     
     func testPlateInformation() {
+
+        let requestExpectation = expectation(description: "Async request")
         
         if let plate = Plate(plate: "ABC-1234") {
             SinespClient().information(for: plate) { (information) in
+                requestExpectation.fulfill()
                 guard let _info = information else {
                     XCTFail()
                     return
@@ -23,6 +26,10 @@ class Tests: XCTestCase {
                 XCTAssertNotNil(_info.plate.numbers)
                 XCTAssertEqual(_info.plate.letters, plate.letters)
                 XCTAssertEqual(_info.plate.numbers, plate.numbers)
+            }
+
+            waitForExpectations(timeout: 30) { (error) in
+                XCTAssertNotNil(error)
             }
         }
         
